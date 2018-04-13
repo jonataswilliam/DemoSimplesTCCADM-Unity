8ad8a29c162ba358495eb1fbdffd6fe79817c297
Jonatas William <jonatas_2602@hotmail.com>
2018-04-12 22:45:29 -0300

    Primeiro Commit

---
+++ Scripts/player.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class player : MonoBehaviour {
+
+	public Rigidbody2D rbPlayer;
+	public float speed;
+	public float movimentoX;
+	public bool walk;
+	public float movimentoY;
+	public Animator playerAnime;
+	public bool pertoLousa = false;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		movimentoX = Input.GetAxisRaw ("Horizontal");
+		movimentoY = Input.GetAxisRaw ("Vertical");
+
+
+		if (movimentoX != 0 || movimentoY != 0) {
+			if (movimentoX != 0) {
+				movimentoY = 0;
+			}
+
+			if (movimentoY != 0) {
+				movimentoX = 0;
+			}
+
+			walk = true;
+		} else {
+			walk = false;
+		}
+
+		// Setando Animação de Andar
+		playerAnime.SetBool ("isWalk", walk);
+
+		if (walk) {
+			rbPlayer.velocity = new Vector2 (movimentoX * speed, movimentoY * speed);
+			playerAnime.SetFloat ("idMovimentoX", movimentoX);
+			playerAnime.SetFloat ("idMovimentoY", movimentoY);
+		} else {
+			rbPlayer.velocity = new Vector2 (movimentoX, movimentoY);
+		}
+
+		if(pertoLousa && Input.GetButton ("Fire1")) {
+			Debug.Log ("Bora para as perguntas");
+			SceneManager.LoadScene ("questions");
+		}
+
+		if(Input.GetButton("Cancel")) {
+			Debug.Log (SceneManager.GetActiveScene().name);
+		}
+
+//		if(SceneManager.GetActiveScene().name == "question" && Input.GetButton ("Cancel")) {
+//			SceneManager.LoadScene ("class1");
+//		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		switch (col.gameObject.name) {
+		case "Faculdade":
+			SceneManager.LoadScene ("school_scene");
+			break;
+
+		case "pisoEntrada":
+			SceneManager.LoadScene ("main_scene");
+			break;
+
+		case "portaSala1":
+			SceneManager.LoadScene("class1");
+			break;	
+
+		case "portaSala3":
+			SceneManager.LoadScene("class2");
+			break;
+
+		case "lousa":
+			pertoLousa = true;
+			break;
+		}
+
+	}
+
+	void OnTriggerExit2D(Collider2D col) {
+		switch (col.gameObject.name) {
+		case "lousa":
+			pertoLousa = false;
+			break;
+		}
+	}
+
+
+}	
+
---
+++ Scripts/selector.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class selector : MonoBehaviour {
+
+    public int resposta = 0;
+    // public Text text;
+    public Transform rbSelector;
+    public GameObject rbA;
+    public GameObject rbB;
+    public GameObject rbC;
+    public GameObject rbD;
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        /// <summary>
+        /// Faz seleção das questoes
+        /// </summary>
+	    if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if(resposta == 0)
+            {
+                resposta = 3;
+                rbSelector.position = new Vector3(rbSelector.transform.position.x, rbD.transform.position.y, 0);
+            }
+            else {
+                --resposta;
+                switch (resposta)
+                {
+                    case 0:
+                        rbSelector.position = new Vector3(rbSelector.transform.position.x, rbA.transform.position.y, 0);
+                        break;
+
+                    case 1:
+                        rbSelector.position = new Vector3(rbSelector.transform.position.x, rbB.transform.position.y, 0);
+                        break;
+
+                    case 2:
+                        rbSelector.position = new Vector3(rbSelector.transform.position.x, rbC.transform.position.y, 0);
+                        break;
+                    
+                }
+                
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (resposta == 3)
+            {
+                resposta = 0;
+                rbSelector.position = new Vector3(rbSelector.transform.position.x, rbA.transform.position.y, 0);
+            }
+            else
+            {
+                ++resposta;
+                switch (resposta)
+                {
+                    case 3:
+                        rbSelector.position = new Vector3(rbSelector.position.x, rbD.transform.position.y, 0);
+                        break;
+
+                    case 1:
+                        rbSelector.position = new Vector3(rbSelector.position.x, rbB.transform.position.y, 0);
+                        break;
+
+                    case 2:
+                        rbSelector.position = new Vector3(rbSelector.position.x, rbC.transform.position.y, 0);
+                        break;
+
+                }
+
+            }
+        }
+
+
+        ///<summary>
+        /// Confirma resposta
+        /// </summary>
+        if(Input.GetButton("Fire1"))
+        {
+            switch(resposta)
+            {
+                case 0:
+//                    text = rbA.GetComponent<Text>();
+//                    text.color = new Color(1.0f, 0, 0, 1.0f);
+                    break;
+
+                case 1:
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+            }
+        }
+
+
+
+
+
+
+    }
+}
